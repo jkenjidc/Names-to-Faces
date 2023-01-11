40881564e02d93692f397cebe9cb8b822a7a3135
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2023-01-11 09:19:23 -0500

    added challenge 1

---
+++ project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by Justine kenji Dela Cruz on 16/12/2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.image = image
+        self.name = name
+    }
+
+}
---
+++ project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Justine kenji Dela Cruz on 15/12/2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+}
